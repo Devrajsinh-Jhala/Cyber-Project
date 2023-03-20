c33329f773a3d5cb3c54d2fe88777da40db30e26
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-03-20 09:46:00 +0530

    Add grants and scholarship in backend

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    images: {
+      domains: ["cdn.sanity.io"],
+    },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,7 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
 import author from './author'
+import grants from './grants'
+import scholarships from './scholarships'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [ grants, scholarships, author, category, blockContent]
