811b186dc6844a8c9c5a944685466031f6d8f0c6
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-03-24 16:06:56 +0530

    Add Pagination

--- components/PostsComponent.tsx
+++ components/PostsComponent.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import urlFor from "@/sanity";
 import ClientSideRoute from "./ClientSideRoute";
+import { usePagination, Pagination } from "pagination-react-js";
 
 type Props = {
   posts: Post[];
@@ -27,6 +28,9 @@ const PostsComponent = ({ posts }: Props) => {
 
   // Search Functionality
   const [search, setSearch] = useState("");
+
+  // Implement Pagination
+  const { currentPage, entriesPerPage, entries } = usePagination(1, 4);
   return (
     <section className="">
       <hr className="border-[#f7ab0a] mb-10" />
@@ -49,7 +53,7 @@ const PostsComponent = ({ posts }: Props) => {
                 activeFilter === item
                   ? "activeFilter text-white bg-blue-500"
                   : ""
-              } tags px-2 py-1 border border-gray-500 rounded-lg cursor-pointer`}
+              } tags px-2 py-1 border hover:text-white hover:bg-blue-500 border-gray-500 rounded-lg cursor-pointer`}
             >
               {item}
             </div>
@@ -65,6 +69,7 @@ const PostsComponent = ({ posts }: Props) => {
               ? post
               : post.title.toLowerCase().includes(search.toLowerCase());
           })
+          .slice(entries.indexOfFirst, entries.indexOfLast)
           .map((post) => (
             <ClientSideRoute
               key={post._id}
@@ -113,6 +118,39 @@ const PostsComponent = ({ posts }: Props) => {
             </ClientSideRoute>
           ))}
       </div>
+      <Pagination
+        entriesPerPage={entriesPerPage.get}
+        totalEntries={filterWorks.length}
+        currentPage={{ get: currentPage.get, set: currentPage.set }}
+        offset={3}
+        classNames={{
+          wrapper: "pagination m-auto",
+          item: "pagination-item",
+          itemActive: "pagination-item-active",
+          navPrev: "pagination-item nav-item",
+          navNext: "pagination-item nav-item",
+          navStart: "pagination-item nav-item",
+          navEnd: "pagination-item nav-item",
+          navPrevCustom: "pagination-item",
+          navNextCustom: "pagination-item",
+        }}
+        showFirstNumberAlways={true}
+        showLastNumberAlways={true}
+        navStart="&#171;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+        navEnd="&#187;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+        navPrev="&#x2039;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+        navNext="&#x203a;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+        navPrevCustom={{
+          steps: 5,
+          content:
+            "Prev" /* Here you can pass anything (Text, HTML Tag, React Component, ...) */,
+        }}
+        navNextCustom={{
+          steps: 5,
+          content:
+            "Next" /* Here you can pass anything (Text, HTML Tag, React Component, ...) */,
+        }}
+      />
     </section>
   );
 };
