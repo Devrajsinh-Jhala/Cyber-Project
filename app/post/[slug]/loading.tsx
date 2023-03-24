88f73bf5311e88a5537638eec7315ea7c0950e30
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-03-24 19:40:20 +0530

    Add loading states

---
+++ app/post/[slug]/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <p className="sm:text-3xl text-base font-bold">
+        Loading the content... Please wait
+      </p>
+    </div>
+  );
+}
